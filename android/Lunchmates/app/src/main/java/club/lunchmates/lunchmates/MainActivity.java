cc31bcedeccafbc2787a576a4a221630534c4487
ミキ Wawrzyniak <mikolajspejs@gmail.com>
2017-06-11 12:52:51 +0200

    Added async task for retrieving the events from the server

--- android/Lunchmates/app/src/main/java/club/lunchmates/lunchmates/MainActivity.java
+++ android/Lunchmates/app/src/main/java/club/lunchmates/lunchmates/MainActivity.java
@@ -2,12 +2,23 @@ package club.lunchmates.lunchmates;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    private static final String BASE_URL = "http://10.0.2.2:9999/";
+    private TextView nearbyNumber;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView t = (TextView) findViewById(R.id.nearbyNumber);
+        new RetrieveEventsTask(t).execute(getAbsoluteUrl("events"));
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
     }
 }
