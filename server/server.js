11f4705862a6077d6cb400d1886d7bc249e0a5e1
Sönke Tölke <sotoe001@stud.fh-dortmund.de>
2017-06-18 12:34:17 +0200

    Add most common REST functions

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http');
 var firebase = require("./firebase.js");
+var db = require('./sql.js');
 
 firebase.initFirebase();
 
@@ -14,26 +15,7 @@ var exampleEvent = {
 
 var events = [];
 
-app.get('/health', function(req,res){
-    res.send('Healthy');
-});
-
-app.get('/events',function(req,res){
-    events.push(exampleEvent);
-    res.send(JSON.stringify(events));
-    console.log("Sent events... " + events.length);
-});
-
-app.get('/createEvent/:ort/:uhrzeit/:datum',function(req,res){
-    var event = {
-        'eventId': events.length,
-        'ort': req.params.ort,
-        'uhrzeit': req.params.uhrzeit,
-        'datum': req.params.datum
-    };
-    events.push(event);
-    res.send(JSON.stringify(events));
-});
+/* ##################################### UTIL ##################################### */
 
 /**
  * Sends a notification to a device with the given token
@@ -51,6 +33,179 @@ app.put('/sendNotification/:token/:message', function (req, res) {
     res.json({status: 200});
 });
 
+app.get('/health', function(req,res){
+    res.send('Healthy');
+});
+
+/* ##################################### EVENTS ##################################### */
+
+/**
+ * Adds an event to the database
+ */
+app.put('/event/add/:name/:x/:y/:time/:author', function (req, res) {
+    var name = req.params.name;
+    var x = req.params.x;
+    var y = req.params.y;
+    var time = new Date(req.params.time * 1000);
+    var author = req.params.author;
+
+    var callback = function (err, data) {
+        if(err) {
+            return res.json({status: 500, error: err});
+        } else {
+            var id = data.insertId;
+            return res.json({status: 200, newId : id});
+        }
+    };
+
+    db.eventAdd(name, x, y, time, author, callback);
+});
+
+app.get('/event/getById/:id', function (req, res) {
+    var id = req.params.id;
+    
+    var callback = function (err, data) {
+        if(err) {
+            console.log(err);
+            return res.json({stauts: 500, error: err});
+        } else {
+            return res.json(data);
+        }
+    };
+
+    db.eventGetById(id, callback);
+});
+
+/**
+ * Returns all active and existing events
+ */
+app.get('/event/getAll', function (req, res) {
+    var callback = function (err, data) {
+        if(err) {
+            console.log(err);
+            return res.json({stauts: 500, error: err});
+        } else {
+            return res.json(data);
+        }
+    };
+
+    db.eventGetAll(callback);
+});
+
+/**
+ * Returns all events created by a given user
+ */
+app.get('/event/getByUser/:id', function (req, res) {
+    var user = req.params.id;
+
+    var callback = function (err, data) {
+        if(err) {
+            console.log(err);
+            return res.json({stauts: 500, error: err});
+        } else {
+            return res.json(data);
+        }
+    };
+
+    db.eventGetByUser(user, callback);
+});
+
+/**
+ * Returns all participants of an given event
+ */
+app.get('/event/getParticipants/:event', function (req, res) {
+    var event = req.params.event;
+
+    var callback = function (err, data) {
+        if(err) {
+            console.log(err);
+            return res.json({stauts: 500, error: err});
+        } else {
+            return res.json(data);
+        }
+    };
+
+    db.eventGetParticipants(event, callback);
+});
+
+/* ##################################### POSITION ##################################### */
+
+/**
+ * Adds a position to the database
+ */
+app.put('/position/add/:x/:y/:author', function (req, res){
+    var x = req.params.x;
+    var y = req.params.y;
+    var author = req.params.author;
+
+    var callback = function (err, data) {
+        if(err) {
+            return res.json({status: 500, error: err});
+        } else {
+            var id = data.insertId;
+            return res.json({status: 200, newId : id});
+        }
+    };
+
+    db.positionAdd(x, y, author, new Date(), callback);
+});
+
+/**
+ * Returns the latest position of a given user
+ */
+app.get('/position/getUsersLatest/:user', function (req, res) {
+    var user = req.params.user;
+
+    var callback = function (err, data) {
+        if(err) {
+            console.log(err);
+            return res.json({stauts: 500, error: err});
+        } else {
+            return res.json(data);
+        }
+    };
+
+    db.positionGetUsersLatest(user, callback);
+});
+
+/**
+ * Returns the latest position of all participants of a given event
+ */
+app.get('/position/getLatestPositionOfParticipants/:event', function (req, res) {
+    var event = req.params.event;
+
+    var callback = function (err, data) {
+        if(err) {
+            console.log(err);
+            return res.json({stauts: 500, error: err});
+        } else {
+            return res.json(data);
+        }
+    };
+
+    db.positionGetLatestPositionOfParticipants(event, callback);
+});
+
+/**
+ * Returns the position with the given id
+ */
+app.get('/position/getById/:id', function (req, res) {
+    var id = req.params.id;
+
+    var callback = function (err, data) {
+        if(err) {
+            console.log(err);
+            return res.json({stauts: 500, error: err});
+        } else {
+            return res.json(data);
+        }
+    };
+
+    db.positionGetById(id, callback);
+});
+
+/* ##################################### USER ##################################### */
+
 /**
  * Updates the firebase token for the user with the given id
  */
@@ -58,8 +213,127 @@ app.put('/user/updateToken/:id/:token', function (req, res) {
     var id = req.params.id;
     var token = req.params.token;
 
-    // TODO: update user in database
+    var callback = function(err, data) {
+        if(err) {
+            return res.json({status: 500, error: err})
+        } else {
+            return res.json({status: 200});
+        }
+    };
+
+    db.updateToken(id, token, callback);
 });
+
+/**
+ * Returns all events of a given user
+ */
+app.get('/user/getEvents/:user', function (req, res) {
+    var user = req.params.user;
+
+    var callback = function (err, data) {
+        if(err) {
+            console.log(err);
+            return res.json({stauts: 500, error: err});
+        } else {
+            return res.json(data);
+        }
+    };
+
+    db.userGetEvents(user, callback);
+});
+
+/**
+ * Returns an user identified by his id
+ */
+app.get('/user/getById/:id', function (req, res) {
+    var id = req.params.id;
+
+    var callback = function (err, data) {
+        if(err) {
+            console.log(err);
+            return res.json({stauts: 500, error: err});
+        } else {
+            return res.json(data);
+        }
+    };
+
+    db.userGetById(id, callback);
+});
+
+/**
+ * Deletes all data associated with the given user
+ */
+app.get('/user/delete/:id', function (req, res) {
+    var id = req.params.id;
+
+    var callback = function (err, data) {
+        if(err) {
+            return res.json({status: 500, error: err})
+        } else {
+            return res.json({status: 200});
+        }
+    };
+
+    db.userDelete(id, callback);
+});
+
+/* ##################################### UsersEvents ##################################### */
+
+/**
+ * Adds an user to an event
+ */
+app.put('/usersEvents/add/:user/:event', function (req, res) {
+    var user = req.params.user;
+    var event = req.params.event;
+
+    var callback = function (err, data) {
+        if(err) {
+            return res.json({status: 500, error: err})
+        } else {
+            var id = data.insertId;
+            return res.json({status: 200, newId : id});
+        }
+    };
+
+    db.usersEventsAdd(user, event, callback);
+});
+
+/**
+ * Removes an UsersEvents identified by it's user and event
+ */
+app.get('/usersEvents/remove/:user/:event', function (req, res) {
+    var user = req.params.user;
+    var event = req.params.event;
+
+    var callback = function (err, data) {
+        if(err) {
+            return res.json({status: 500, error: err})
+        } else {
+            return res.json({status: 200});
+        }
+    };
+
+    db.usersEventsRemove(user, event, callback);
+});
+
+/**
+ * Returns the UsersEvents with the given id
+ */
+app.get('/usersEvents/getById/:id', function (req, res) {
+    var id = req.params.id;
+
+    var callback = function (err, data) {
+        if(err) {
+            return res.json({status: 500, error: err})
+        } else {
+            return res.json(data);
+        }
+    };
+
+    db.usersEventsGetById(id, callback);
+});
+
+/* ##################################### ServerStart ##################################### */
 
 var server = app.listen(9999,'127.0.0.1',function(){
     var host = server.address().address;
---
+++ server/sql.js
@@ -0,0 +1,131 @@
+var mysql = require("mysql");
+var config = require("./config/db.json");
+
+var con = mysql.createConnection({
+    host: config.host,
+    user: config.username,
+    password: config.password,
+    database: config.db
+});
+
+exports.updateToken = function (id, token, callback) {
+    con.query("UPDATE Users SET token = ? WHERE id = ?;", [token, id], function (err, result) {
+       callback(err, result);
+    });
+};
+
+
+/* ##################################### POSITION ##################################### */
+
+exports.positionAdd = function (x, y, author, time, callback) {
+    con.query("INSERT INTO Positions(x, y, user, time) VALUES(?,?,?,?);",[x,y,author,time], function (err, result) {
+        callback(err, result);
+    });
+};
+
+exports.positionGetUsersLatest = function (user, callback) {
+    con.query("SELECT * FROM Positions WHERE user = ? ORDER BY time DESC LIMIT 1;", [user], function (err, result) {
+        callback(err, result);
+    });
+};
+
+exports.positionGetLatestPositionOfParticipants = function (event, callback) {
+    con.query("SELECT * FROM Positions WHERE id IN (SELECT max(p.id) FROM Positions p GROUP BY p.user HAVING p.user IN (SELECT ue.user FROM UsersEvents ue WHERE ue.event = ?));", [event], function (err, result) {
+        callback(err, result);
+    });
+};
+
+exports.positionGetById = function (id, callback) {
+    con.query("SELECT * FROM Positions WHERE id = ?;", [id], function (err, result) {
+        callback(err, result);
+    });
+};
+
+/* ##################################### EVENTS ##################################### */
+
+exports.eventAdd = function (name, x, y, time, author, callback) {
+    con.query("INSERT INTO Events (name, x, y, date, author) VALUES(?,?,?,?,?);" , [name, x, y, time, author], function (err, result) {
+        callback(err, result);
+    });
+};
+
+exports.eventGetAll = function (callback) {
+    con.query("SELECT * FROM Events LEFT JOIN Users u ON author = u.id WHERE date > now();", function (err, result) {
+        callback(err, result);
+    });
+};
+
+exports.eventGetByUser = function (user, callback) {
+    con.query("SELECT * FROM Events WHERE date > now() AND author = ?;", [user], function (err, result) {
+        callback(err, result);
+    });
+};
+
+exports.eventGetParticipants = function (event, callback) {
+    con.query("SELECT u.* FROM UsersEvents ue LEFT JOIN Users u ON ue.user = u.id WHERE ue.event = ?;", [event], function(err, result) {
+        callback(err, result);
+    });
+};
+
+exports.eventGetById = function (id, callback) {
+    con.query("SELECT * FROM Events WHERE id = :id;", [id], function (err, result) {
+        callback(err, result);
+    });
+};
+
+/* ##################################### USER ##################################### */
+
+exports.userGetEvents = function (user, callback) {
+    con.query("SELECT * FROM Events WHERE author = ?;", [user], function (err, result) {
+       callback(err, result);
+    });
+};
+
+exports.userGetById = function (id, callback) {
+    con.query("SELECT * FROM Users WHERE id = ?;", [id], function (err, result) {
+        callback(err, result);
+    });
+};
+
+exports.userDelete = function (id, callback) {
+    con.query("DELETE FROM UsersEvents WHERE user = ? OR event IN (SELECT id FROM Events WHERE author = ?);", [id, id], function (err, data) {
+        if(err) {
+            callback(err);
+        } else {
+            con.query("DELETE FROM Positions WHERE user = ?;", [id], function (err, data) {
+                if(err) {
+                    callback(err);
+                } else {
+                    con.query("DELETE FROM Events WHERE author = ?;", [id], function (err, data) {
+                        if(err) {
+                            callback(err);
+                        } else {
+                            con.query("DELETE FROM Users WHERE id = ?;", [id], function (err, data) {
+                                callback(err, data);
+                            });
+                        }
+                    });
+                }
+            });
+        }
+    });
+};
+
+/* ##################################### UsersEvents ##################################### */
+exports.usersEventsAdd = function (user, event, callback) {
+    con.query("INSERT INTO UsersEvents(user, event) VALUES(?, ?);", [user, event], function (err, result) {
+        callback(err, result);
+    });
+};
+
+exports.usersEventsRemove = function (user, event, callback) {
+    con.query("REMOVE FROM UsersEvents WHERE event = ? AND user = ?;", [event, user], function (err, result) {
+        callback(err, result);
+    });
+};
+
+exports.usersEventsGetById = function (id, callback) {
+    con.query("SELECT * FROM UsersEvents WHERE id = ?;", [id], function (err, result) {
+        callback(err, result);
+    });
+};
