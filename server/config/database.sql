f2a5668f45a083ae8511d7cff0fbb53781ba52c4
Jonas Fleck <jonas@flecknet.de>
2017-06-27 17:49:56 +0200

    Change mysql create table order

--- server/config/database.sql
+++ server/config/database.sql
@@ -1,3 +1,11 @@
+CREATE TABLE Users
+(
+    id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    token VARCHAR(255),
+    session_token VARCHAR(255),
+    name VARCHAR(50),
+    email VARCHAR(100)
+);
 CREATE TABLE Events
 (
     name VARCHAR(100),
@@ -19,15 +27,6 @@ CREATE TABLE Positions
     CONSTRAINT Positions_user_fk FOREIGN KEY (user) REFERENCES Users (id)
 );
 CREATE INDEX Positions_user_fk ON Positions (user);
-
-CREATE TABLE Users
-(
-    id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    token VARCHAR(255),
-    session_token VARCHAR(255),
-    name VARCHAR(50),
-    email VARCHAR(100)
-);
 
 CREATE TABLE UsersEvents
 (
