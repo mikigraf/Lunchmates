97caea979e80c18dafd6a267506f6de8193614e5
Jonas Fleck <jonas@flecknet.de>
2017-06-27 17:53:14 +0200

    Add google auth in client

--- android/Lunchmates/app/src/main/java/club/lunchmates/lunchmates/MainActivity.java
+++ android/Lunchmates/app/src/main/java/club/lunchmates/lunchmates/MainActivity.java
@@ -1,8 +1,10 @@
 package club.lunchmates.lunchmates;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
@@ -17,7 +19,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -26,9 +35,19 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import java.util.List;
 
+import club.lunchmates.lunchmates.controller.PreferencesControllerImpl;
+import club.lunchmates.lunchmates.controller.interfaces.PreferencesController;
+import club.lunchmates.lunchmates.data.AuthenticationResult;
 import club.lunchmates.lunchmates.data.Event;
 import club.lunchmates.lunchmates.rest.RestHelperImpl;
 import club.lunchmates.lunchmates.rest.interfaces.RestHelper;
@@ -36,17 +55,19 @@ import club.lunchmates.lunchmates.rest.interfaces.RestHelper;
 import static android.R.attr.value;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback, GoogleApiClient.OnConnectionFailedListener {
     private static final String BASE_URL = "http://10.0.2.2:9999/";
+    private static final int RESULT_CODE_LOGIN = 815;
     private TextView nearbyNumber;
 
     private GoogleMap mMap;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mAuth = FirebaseAuth.getInstance();
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -86,10 +107,63 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        signIn();
     }
 
     private static String getAbsoluteUrl(String relativeUrl) {
         return BASE_URL + relativeUrl;
+    }
+
+    private void signIn() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .requestProfile()
+                .build();
+        GoogleApiClient mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RESULT_CODE_LOGIN);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RESULT_CODE_LOGIN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if(result.isSuccess()) {
+                sendGoogleLoginToken(result.getSignInAccount().getIdToken());
+            }
+        }
+    }
+
+    private void sendGoogleLoginToken(String token) {
+        RestHelper helper = new RestHelperImpl();
+        RestHelper.DataReceivedListener<AuthenticationResult> listener = new RestHelper.DataReceivedListener<AuthenticationResult>() {
+            @Override
+            public void onDataReceived(AuthenticationResult result) {
+                if(result == null) {
+                    Toast.makeText(MainActivity.this, "Login connection error", Toast.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    if(!result.isSuccess()) {
+                        Toast.makeText(MainActivity.this, "Server error", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    PreferencesController prefs = new PreferencesControllerImpl(MainActivity.this);
+                    Log.d("bla", "token" + result.getSessionToken());
+                    prefs.setSessionToken(result.getSessionToken());
+                    prefs.setUserId(result.getUserId());
+
+                }
+            }
+        };
+        helper.login(token, listener);
     }
 
     @Override
@@ -157,5 +231,10 @@ public class MainActivity extends AppCompatActivity
         }
         //mMap.setMyLocationEnabled(true);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(fhDO, 15.0f));
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
